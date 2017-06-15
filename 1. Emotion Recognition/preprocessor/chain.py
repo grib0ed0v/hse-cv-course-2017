890e3c12c3faf94173462b0b7782151099ab3e51
Вико <maviko@edu.hse.ru>
2017-06-15 23:29:48 +0300

    add tonal correction

--- 1. Emotion Recognition/preprocessor/chain.py
+++ 1. Emotion Recognition/preprocessor/chain.py
@@ -1,5 +1,5 @@
 # Class describes sequence of processor, which should be applied to the image
-from preprocessor.processor.impl.color import ColorProcessor
+from preprocessor.processor.impl.tonal import TonalProcessor
 from preprocessor.processor.impl.noise import NoiseProcessor
 import cv2
 
@@ -9,7 +9,7 @@ class ProcessorChain:
     def __init__(self):
         self.chain = list()
         self.chain.append(NoiseProcessor())
-        self.chain.append(ColorProcessor())
+        self.chain.append(TonalProcessor())
         # add more processors here
 
     # main method for running pre process actions
